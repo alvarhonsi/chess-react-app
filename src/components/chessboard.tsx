be6a8af2f7820641fd561a5379d5a3f7fc5fca52
Alvar Hønsi <alvarhonsi@gmail.com>
2021-07-06 17:22:45 +0200

    piece images

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import {Grid, GridItem} from '@chakra-ui/react'
+import Image from 'next/image'
+import {Grid, GridItem, Box} from '@chakra-ui/react'
+import Pieces from './pieces'
+import bishop_black from '../../public/chess-pieces/bishop_b.png'
 
 const board_columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 const board_rows = ['1', '2', '3', '4', '5', '6', '7', '8']
@@ -28,8 +31,14 @@ const generate_grid = () => {
     for(let j = board_rows.length-1; j >= 0; j--) {
         for(let i = 0; i < board_columns.length; i++) {
             tiles.push(
-                <GridItem key={count++} bg={(i + j + 2) % 2 === 0 ? board_colors.primary : board_colors.secondary}>
-
+                <GridItem 
+                    key={count++} 
+                    bg={(i + j + 2) % 2 === 0 ? board_colors.primary : board_colors.secondary}
+                    display="flex" 
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Image src={bishop_black} width="70" height="70" objectFit='contain'/>
                 </GridItem>
             )
         }
---
+++ src/components/pieces.ts
@@ -0,0 +1,16 @@
+const Pieces = {
+    bishop_black : '/chess-pieces/bishop_b.png',
+    bishop_white : '/chess-pieces/bishop_w.png',
+    king_black : '/chess-pieces/king_b.png',
+    king_white : '/chess-pieces/king_w.png',
+    knight_black : '/chess-pieces/knight_b.png',
+    knight_white : '/chess-pieces/knight_w.png',
+    pawn_black : '/chess-pieces/pawn_b.png',
+    pawn_white : '/chess-pieces/pawn_w.png',
+    queen_black : '/chess-pieces/queen_b.png',
+    queen_white : '/chess-pieces/queen_w.png',
+    rook_black : '/chess-pieces/rook_b.png',
+    rook_white : '/chess-pieces/rook_w.png'
+}
+
+export default Pieces
