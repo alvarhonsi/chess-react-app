b31de769b4c8ac25f573e3e9260b3a0ecf2155b8
Alvar Hønsi <alvarhonsi@gmail.com>
2023-03-08 13:06:18 +0100

    fix index

--- src/components/chessboard/board-tiles.tsx
+++ src/components/chessboard/board-tiles.tsx
@@ -1,127 +1,134 @@
-import {GridItem, Box} from '@chakra-ui/react'
-
+import { GridItem, Box } from "@chakra-ui/react";
 
 interface PieceProps {
-    index : number,
-    image? : string,
-    selectable : boolean,
+  index: number;
+  image?: string;
+  selectable: boolean;
 }
 
-export const BoardPiece = ({index, image, selectable} : PieceProps) => {
-    return (
+export const BoardPiece = ({ index, image, selectable }: PieceProps) => {
+  return (
     <GridItem
-        display="flex" 
-        alignItems="center"
-        justifyContent="center"
-        w="100%"
-        h="100%"
-        className='grid-tile'
-        index={index}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      w="100%"
+      h="100%"
+      className="grid-tile"
+      data-index={index}
     >
-        {image && <Box
-            w="100%"
-            h="100%"
-            backgroundImage={image}
-            bgRepeat="no-repeat"
-            bgPosition="center"
-            className="chess-piece"
-            _hover={selectable ? {
-                cursor: 'pointer'
-            } : {}}
-        >
-        </Box>}
+      {image && (
+        <Box
+          w="100%"
+          h="100%"
+          backgroundImage={image}
+          bgRepeat="no-repeat"
+          bgPosition="center"
+          className="chess-piece"
+          _hover={
+            selectable
+              ? {
+                  cursor: "pointer",
+                }
+              : {}
+          }
+        ></Box>
+      )}
     </GridItem>
-    )
-}
-
+  );
+};
 
 interface TileProps {
-    number: number
-    board_colors : {
-        primary : string,
-        secondary : string
-    }
-    tileEffect : Effect
+  number: number;
+  board_colors: {
+    primary: string;
+    secondary: string;
+  };
+  tileEffect: Effect;
 }
 
-export const BoardTile = ({number, board_colors, tileEffect} : TileProps) => {
-    return (
+export const BoardTile = ({ number, board_colors, tileEffect }: TileProps) => {
+  return (
     <GridItem
-        bg={number % 2 === 0 ? board_colors.secondary : board_colors.primary}
-        display="flex" 
-        alignItems="center"
-        justifyContent="center"
-        w="100%"
-        h="100%"
+      bg={number % 2 === 0 ? board_colors.secondary : board_colors.primary}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      w="100%"
+      h="100%"
     >
-        {TileEffect(tileEffect)}
+      {TileEffect(tileEffect)}
     </GridItem>
-    )
-}
+  );
+};
 
 export enum Effect {
-    None,
-    Origin,
-    End,
-    Movable,
-    Attack,
-    Check,
+  None,
+  Origin,
+  End,
+  Movable,
+  Attack,
+  Check,
 }
 
-const TileEffect = (effect : Effect) => {
-    let styleProps: any = {}
-    switch (effect) {
-        case Effect.Origin:
-            styleProps = {
-                bg : 'yellow.200',
-                w: '100%',
-                h: '100%',
-                cursor: 'pointer',
-                opacity: 0.7
-            }
-            break
-        case Effect.End:
-            styleProps = {
-                bg : 'yellow.300',
-                w: '100%',
-                h: '100%',
-                cursor: 'pointer',
-                opacity: 1
-            }
-            break
-        case Effect.Movable:
-            styleProps = {
-                bg : 'green.800',
-                w: '20%',
-                h: '20%',
-                borderRadius : '50%',
-                cursor: 'pointer',
-                opacity: 0.5
-            }
-            break
-        case Effect.Attack:
-            styleProps = {
-                bg : 'red.500',
-                w: '50%',
-                h: '50%',
-                borderRadius : '50%',
-                cursor: 'pointer',
-                opacity: 0.7
-            }
-            break
-        case Effect.Check:
-            styleProps = {
-                bg : 'red.500',
-                w: '70%',
-                h: '70%',
-                borderRadius : '50%',
-                cursor: 'pointer',
-                opacity: 0.7
-            }
-            break
-        default:
-            break
-    }
+const TileEffect = (effect: Effect) => {
+  let styleProps: any = {};
+  switch (effect) {
+    case Effect.Origin:
+      styleProps = {
+        bg: "yellow.200",
+        w: "100%",
+        h: "100%",
+        cursor: "pointer",
+        opacity: 0.7,
+      };
+      break;
+    case Effect.End:
+      styleProps = {
+        bg: "yellow.300",
+        w: "100%",
+        h: "100%",
+        cursor: "pointer",
+        opacity: 1,
+      };
+      break;
+    case Effect.Movable:
+      styleProps = {
+        bg: "green.800",
+        w: "20%",
+        h: "20%",
+        borderRadius: "50%",
+        cursor: "pointer",
+        opacity: 0.5,
+      };
+      break;
+    case Effect.Attack:
+      styleProps = {
+        bg: "red.500",
+        w: "50%",
+        h: "50%",
+        borderRadius: "50%",
+        cursor: "pointer",
+        opacity: 0.7,
+      };
+      break;
+    case Effect.Check:
+      styleProps = {
+        bg: "red.500",
+        w: "70%",
+        h: "70%",
+        borderRadius: "50%",
+        cursor: "pointer",
+        opacity: 0.7,
+      };
+      break;
+    default:
+      break;
+  }
 
-    return <Box className='tile-effect' {...styleProps}> </Box>
-}
+  return (
+    <Box className="tile-effect" {...styleProps}>
+      {" "}
+    </Box>
+  );
+};
--- src/components/chessboard/chessboard.tsx
+++ src/components/chessboard/chessboard.tsx
@@ -1,147 +1,169 @@
-import React, {useRef, useState} from 'react'
-import {Grid, Flex, Box, Text, VStack, Button} from '@chakra-ui/react'
-import {piece_images, getMoves, getAttacks, pieceIsMovable, isInCheck, isCapturable, getKingPos} from './pieces'
-import {BoardTile, BoardPiece, Effect} from './board-tiles'
-import {BoardState, CastleAvailability, Piece, ToMove} from './board-types'
-import {castleAvailability} from '../test-positions'
-import {boardPositionToIndex, indexToBoardPosition, nextToMove} from './utils'
-import {readFEN} from '../fen'
-const clone = require('rfdc')()
+import React, { useRef, useState } from "react";
+import { Grid, Flex, Box, Text, VStack, Button } from "@chakra-ui/react";
+import {
+  piece_images,
+  getMoves,
+  getAttacks,
+  pieceIsMovable,
+  isInCheck,
+  isCapturable,
+  getKingPos,
+} from "./pieces";
+import { BoardTile, BoardPiece, Effect } from "./board-tiles";
+import { BoardState, CastleAvailability, Piece, ToMove } from "./board-types";
+import { castleAvailability } from "../test-positions";
+import {
+  boardPositionToIndex,
+  indexToBoardPosition,
+  nextToMove,
+} from "./utils";
+import { readFEN } from "../fen";
+const clone = require("rfdc")();
 
-const startFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+const startFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-const board_columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-const board_rows = ['8', '7', '6', '5', '4', '3', '2', '1']
+const board_columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const board_rows = ["8", "7", "6", "5", "4", "3", "2", "1"];
 const board_colors = {
-    primary: 'yellow.500', 
-    secondary: 'yellow.700'
-}
-const board_width = [400, null, 600, null, 800]
-const board_height = [400, null, 600, null, 800]
+  primary: "yellow.500",
+  secondary: "yellow.700",
+};
+const board_width = [400, null, 600, null, 800];
+const board_height = [400, null, 600, null, 800];
 
 interface Props {
-    boardState : BoardState,
-    effectGrid : Effect[],
-    handleClick : (e: React.MouseEvent) => Promise<void>
+  boardState: BoardState;
+  effectGrid: Effect[];
+  handleClick: (e: React.MouseEvent) => Promise<void>;
 }
 
-const ChessBoard = ({boardState, effectGrid, handleClick} : Props) => {
-    const chessBoardRef = useRef<HTMLDivElement>(null)
-    const getBoardLayer = () => chessBoardRef.current?.firstChild?.firstChild as HTMLDivElement
+const ChessBoard = ({ boardState, effectGrid, handleClick }: Props) => {
+  const chessBoardRef = useRef<HTMLDivElement>(null);
+  const getBoardLayer = () =>
+    chessBoardRef.current?.firstChild?.firstChild as HTMLDivElement;
 
-    return (
-        <Box
-            h={board_height}
-            w={board_width}
-            colSpan={4}
-            onMouseDown={e => handleClick(e)}
-            position='relative'
-            shadow='xl'
-        >
-            <Grid
-                h={board_height}
-                w={board_width}
-                templateColumns='repeat(8, 1fr)'
-                templateRows='repeat(8, 1fr)'
-                gap={0}
+  return (
+    <Box
+      h={board_height}
+      w={board_width}
+      onMouseDown={(e) => handleClick(e)}
+      position="relative"
+      shadow="xl"
+    >
+      <Grid
+        h={board_height}
+        w={board_width}
+        templateColumns="repeat(8, 1fr)"
+        templateRows="repeat(8, 1fr)"
+        gap={0}
+      >
+        {effectGrid && generateBoard(effectGrid)}
+      </Grid>
+      <Grid
+        h={board_height}
+        w={board_width}
+        templateColumns="repeat(8, 1fr)"
+        templateRows="repeat(8, 1fr)"
+        gap={0}
+        position="absolute"
+        top={0}
+        left={0}
+      >
+        {boardState && generatePieceLayer(boardState)}
+      </Grid>
+      <Grid
+        h={board_height}
+        w={"20px"}
+        templateColumns="repeat(1, 1fr)"
+        templateRows="repeat(8, 1fr)"
+        gap={3}
+        position="absolute"
+        left={"-50px"}
+        top={0}
+      >
+        {board_rows.map((row) => {
+          return (
+            <Text
+              key={row}
+              textAlign="end"
+              textColor="white"
+              fontSize={["lg", null, "xl", null, "2xl"]}
+              marginY="auto"
             >
-                {effectGrid && generateBoard(effectGrid)}
-            </Grid>
-            <Grid
-                h={board_height}
-                w={board_width}
-                templateColumns='repeat(8, 1fr)'
-                templateRows='repeat(8, 1fr)'
-                gap={0}
-                position='absolute'
-                top={0}
-                left={0}
+              {row}
+            </Text>
+          );
+        })}
+      </Grid>
+      <Grid
+        h={"30px"}
+        w={board_width}
+        templateColumns="repeat(8, 1fr)"
+        templateRows="repeat(1, 1fr)"
+        gap={3}
+        position="absolute"
+        left={0}
+        bottom={"-35px"}
+      >
+        {board_columns.map((column) => {
+          return (
+            <Text
+              key={column}
+              textAlign="center"
+              textColor="white"
+              fontSize={["lg", null, "xl", null, "2xl"]}
             >
-                {boardState && generatePieceLayer( boardState )}
-            </Grid>
-            <Grid
-                h={board_height}
-                w={'20px'}
-                templateColumns='repeat(1, 1fr)'
-                templateRows='repeat(8, 1fr)'
-                gap={3}
-                position='absolute'
-                left={'-50px'}
-                top={0}
-            >
-                {board_rows.map(row => {
-                    return (
-                        <Text
-                            key={row}
-                            textAlign='end'
-                            textColor='white'
-                            fontSize={['lg', null, 'xl', null, '2xl']}
-                            marginY='auto'
-                        >
-                            {row}
-                        </Text>
-                    )
-                })}
-            </Grid>
-            <Grid
-                h={'30px'}
-                w={board_width}
-                templateColumns='repeat(8, 1fr)'
-                templateRows='repeat(1, 1fr)'
-                gap={3}
-                position='absolute'
-                left={0}
-                bottom={'-35px'}
-            >
-                {board_columns.map(column => {
-                    return (
-                        <Text
-                            key={column}
-                            textAlign='center' 
-                            textColor='white'
-                            fontSize={['lg', null, 'xl', null, '2xl']}
-                        >
-                            {column}
-                        </Text>
-                    )
-                })}
-            </Grid>
-        </Box>
-    )
-}
+              {column}
+            </Text>
+          );
+        })}
+      </Grid>
+    </Box>
+  );
+};
 
-const generateBoard = (effectGrid : Effect[]) => {
-    let tiles = []
-    let index = 0
-    for(let j = board_rows.length-1; j >= 0; j--) {
-        for(let i = 0; i < board_columns.length; i++) {
-            const num = i + j;
-            tiles.push(
-                <BoardTile key={index} tileEffect={effectGrid[index]} number={num} board_colors={board_colors}/>
-            )
-            index++
-        }
+const generateBoard = (effectGrid: Effect[]) => {
+  let tiles = [];
+  let index = 0;
+  for (let j = board_rows.length - 1; j >= 0; j--) {
+    for (let i = 0; i < board_columns.length; i++) {
+      const num = i + j;
+      tiles.push(
+        <BoardTile
+          key={index}
+          tileEffect={effectGrid[index]}
+          number={num}
+          board_colors={board_colors}
+        />
+      );
+      index++;
     }
+  }
 
-    return tiles
-}
+  return tiles;
+};
 
 //Generates a grid of BoardPieces as the piece layer
-const generatePieceLayer = (boardState : BoardState) => {
-    const pieces = boardState.pieces
-    let tiles = []
-    let count = 0
-    for(let j = board_rows.length-1; j >= 0; j--) {
-        for(let i = 0; i < board_columns.length; i++) {
-            const piece = (pieces[count] as Piece)
-            tiles.push(
-                <BoardPiece key={count} selectable={pieceIsMovable(piece, boardState.toMove)} index={count} image={piece_images[piece]}/>
-            )
-            count++
-        }
+const generatePieceLayer = (boardState: BoardState) => {
+  const pieces = boardState.pieces;
+  let tiles = [];
+  let count = 0;
+  for (let j = board_rows.length - 1; j >= 0; j--) {
+    for (let i = 0; i < board_columns.length; i++) {
+      const piece = pieces[count] as Piece;
+      tiles.push(
+        <BoardPiece
+          key={count}
+          selectable={pieceIsMovable(piece, boardState.toMove)}
+          index={count}
+          image={piece_images[piece]}
+        />
+      );
+      count++;
     }
+  }
 
-    return tiles
-}
+  return tiles;
+};
 
-export default ChessBoard
+export default ChessBoard;
